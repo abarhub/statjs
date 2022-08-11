89fd3005315c2a480667f4aa13de3169a75c79da
abarhub <abarhub@gmail.com>
2022-08-11 13:23:27 +0200

    initialisation

---
+++ stat.js
@@ -0,0 +1,46 @@
+"use strict";
+
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+
+
+function test1() {
+    fetch('data/user_biometrics.json')
+        .then(function (response) {
+            let res = response.json();
+            return res;
+        });
+}
+
+//test1();
+
+console.log("size="+dataJson.length);
+
